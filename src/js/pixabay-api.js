b440633677e808176b2ca389e904015cfe557c4f
OlgaKozoriz07 <161890591+OlgaKozoriz07@users.noreply.github.com>
2024-08-28 18:01:04 +0200

    markup axios

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api';
+
+export const fetchPhotos = (searchedQuery, page) => {
+const axiosOptions = {
+    params: {
+    key: '45531869-402fff4ce98a39e1a3b7e2442',
+    q: searchedQuery,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: page,
+    per_page: 15,
+    },
+};
+return axios.get(`/`, axiosOptions);
+};
